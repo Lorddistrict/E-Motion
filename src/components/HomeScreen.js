6fd6271844b5cbc0785757b7f79790e75ea6971e
Lorddistrict <etienne.crespi@laposte.net>
2019-10-04 06:56:40 +0200

    Start adding API fetching

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
-import { Button, View, Text } from 'react-native';
+import { View, ScrollView, Text, StyleSheet } from 'react-native';
 
-export default class Homescreen extends Component {
+export default class HomeScreen extends Component {
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Home Screen</Text>
-                <Button
-                    title="Go to About"
-                    onPress={() => this.props.navigation.navigate('About')}
-                />
+            <View style={styles.container}>
+                <ScrollView>
+                    <Text style={styles.vehicleListHeader}>Welcome</Text>
+                </ScrollView>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    welcome: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+    },
+    instructions: {
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 5,
+    },
+    vehicleListHeader: {
+      fontSize: 20,
+      color: '#000',
+      textTransform: 'uppercase',
+      marginTop: '60%',
+    }
+  });
--- src/components/ProfileScreen.js
+++ src/components/ProfileScreen.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 export default class ContactScreen extends Component {
     render() {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text>Profile Screen</Text>
+                <Button
+                    title="Go to Home"
+                    onPress={() => this.props.navigation.navigate('Home')}
+                />
             </View>
         )
     }
