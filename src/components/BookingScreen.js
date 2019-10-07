ce77a8386fbfbdcf431435cedd13b9de3493e30f
Lorddistrict <etienne.crespi@laposte.net>
2019-10-07 22:31:31 +0200

    Add API listing

--- src/components/BookingScreen.js
+++ src/components/BookingScreen.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native';
+import VehicleList from './VehicleList';
 
 export default class ContactScreen extends Component {
     render() {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text>Booking Screen</Text>
+                <VehicleList />
             </View>
         )
     }
--- src/components/VehicleList.js
+++ src/components/VehicleList.js
@@ -1,14 +1,88 @@
 import React from 'react';
-import { View, Text, Stylesheet } from 'react-native';
+import { 
+    StyleSheet,
+    View,
+    ActivityIndicator,
+    FlatList,
+    Text,
+    TouchableOpacity
+} from 'react-native';
 
 export default class VehicleList extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+            dataSource : []
+        };
+    }
+
+    async componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then((responseJson) => {
+            this.setState({
+                loading: false,
+                dataSource: responseJson
+            })
+        })
+        .catch(error => console.log(error))
+    }
+
+    FlatListItemSeparator = () => {
+        return (
+            <View style={{
+                height: .5,
+                width:"100%",
+                backgroundColor:"rgba(0,0,0,0.5)",
+            }}
+            />
+        );
+    }
+
+    renderItem = (data) =>
+        <TouchableOpacity style={styles.list}>
+            <Text style={styles.lightText}>{data.item.name}</Text>
+            <Text style={styles.lightText}>{data.item.email}</Text>
+            <Text style={styles.lightText}>{data.item.company.name}</Text>
+        </TouchableOpacity>
+
     render() {
-        return '';
+        if (this.state.loading) {
+            return(
+                <View style={styles.loader}> 
+                    <ActivityIndicator size="large" color="#0c9"/>
+                </View>
+            )
+        }
+        return(
+            <View style={styles.container}>
+                <FlatList
+                    data = {this.state.dataSource}
+                    ItemSeparatorComponent = {this.FlatListItemSeparator}
+                    renderItem = {item => this.renderItem(item)}
+                    keyExtractor = {item => item.id.toString()}
+                />
+            </View>
+        )
     }
 }
 
-const styles = Stylesheet.create({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: "#fff"
+    },
+    loader:{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff"
+    },
+    list:{
+        paddingVertical: 4,
+        margin: 5,
+        backgroundColor: "#fff"
     }
 });
